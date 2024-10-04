bd4acbfa827fc3b2c3cd9f965c02093393535c31
Amartya Varma <amartya.varma@gmail.com>
2024-10-04 02:55:19 -0700

    f

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "2.3m-test-0"
 project_name = "3dups"
-dataset_id = "amuvarma/2.2-wdups-tts-0"
+dataset_id = "amuvarma/2.2-wdups-tts-1"
 
 model_name = "google/gemma-2-2b"
 tokenizer_name = "google/gemma-2-2b"

fbdbbc818af4641a47257e116f2e9c707cb213c8
Amartya Varma <amartya.varma@gmail.com>
2024-10-06 23:58:56 -0700

    f

--- upload_model.py
+++ upload_model.py
@@ -37,4 +37,4 @@ def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):
         return None
 
 
-push_folder_to_hub(f"./{base_repo_id}", f"amuvarma/gemma-wdups-1node-checkpoint-44000", "Update model")
+push_folder_to_hub(f"./{base_repo_id}", f"amuvarma/gemma-wdups-2node-checkpoint-60000", "Update model")

4526a5253ab92acf1b7629e69bd736b2ab621646
Amartya Varma <amartya.varma@gmail.com>
2024-10-08 13:23:03 -0700

    f

--- finetune.py
+++ finetune.py
@@ -36,6 +36,7 @@ training_args = TrainingArguments(
     output_dir="./results",
     num_train_epochs=3,
     per_device_train_batch_size=4,
+    logging_steps=20,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir="./logs",

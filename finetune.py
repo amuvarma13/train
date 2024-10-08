274d05befa28e93896060e74c1c3dd7b033965b5
Amartya Varma <amartya.varma@gmail.com>
2024-10-08 13:12:03 -0700

    f

---
+++ finetune.py
@@ -0,0 +1,55 @@
+from datasets import load_dataset
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
+from peft import get_peft_model, LoraConfig, TaskType
+from datasets import Dataset
+
+dsn = "amuvarma/24k-gcp-llama"
+ds = load_dataset(dsn)
+
+
+
+# Load the pre-trained model and tokenizer
+model_name = "amuvarma/llama-checkpoint-180000"  # Replace with your model
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+
+
+# Define LoRA configuration
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1,
+    bias="none",
+    target_modules=["q_proj", "v_proj", "k_proj", "o_proj" ]
+)
+
+# Apply LoRA to the model
+model = get_peft_model(model, peft_config)
+
+# Prepare your dataset
+
+
+# Define training arguments
+training_args = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir="./logs",
+)
+
+# Initialize Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=ds["train"],
+)
+
+# Start training
+trainer.train()
+
+# Save the fine-tuned model
+model.save_pretrained("./fine_tuned_model")

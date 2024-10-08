e30ab17fb4bd5f25a67cb80b85a1a960bf9b4f36
Amartya Varma <amartya.varma@gmail.com>
2024-10-08 14:38:07 -0700

    f

--- finetune.py
+++ finetune.py
@@ -28,7 +28,13 @@ peft_config = LoraConfig(
 # Apply LoRA to the model
 model = get_peft_model(model, peft_config)
 
-# Prepare your dataset
+trainable_params = 0
+all_param = 0
+for _, param in model.named_parameters():
+    all_param += param.numel()
+    if param.requires_grad:
+        trainable_params += param.numel()
+print(f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}")
 
 
 # Define training arguments

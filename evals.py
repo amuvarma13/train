fa519fd7f957b3c9fa0d301241ece3b0ca983cbc
Amartya Varma <amartya.varma@gmail.com>
2024-10-11 02:50:18 -0700

    f

--- evals.py
+++ evals.py
@@ -13,7 +13,7 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "2.3m-test-0"
 project_name = "3dups"
-dsn = "amuvarma/5k-llama-wdups"
+dsn = "amuvarma/conversation_text-tune-13k-25kl"
 
 model_name = "meta-llama/Llama-3.2-3B" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"

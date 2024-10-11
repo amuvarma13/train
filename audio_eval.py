81a525fd8ce080e394d0a42ce40271b92cdc2c90
Amartya Varma <amartya.varma@gmail.com>
2024-10-11 03:07:44 -0700

    f

--- audio_eval.py
+++ audio_eval.py
@@ -1,37 +1,109 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
+from transformers import AutoModelForCausalLM, Trainer, TrainingArguments, AutoTokenizer
+import numpy as np
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullStateDictConfig
+from torch.distributed.fsdp import ( FullyShardedDataParallel as FSDP, FullStateDictConfig, StateDictType)
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+import os
+import wandb
+from huggingface_hub import HfApi, create_repo
 
-# Step 1: Load the model and tokenizer
+
+base_repo_id = "2.3m-test-0"
+project_name = "3dups"
+dsn = "amuvarma/audio_eval_small"
+
 model_name = "amuvarma/convo-llama-13k-text"
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, )
+tokenizer_name = "meta-llama/Llama-3.2-3B"
+epochs = 1
+batch_size = 1
+pad_token = 128263
+save_steps = 10000
 
-# Step 2: Load the dataset
-dataset = load_dataset("amuvarma/audio_eval_small")
 
-# Step 3: Create DataLoader
-dataloader = DataLoader(dataset["train"], batch_size=1)
+wandb.init(
+    project="audio_only_tests", 
+    name = "full_audio_pretrain"
+    )
+ 
+ 
 
-# Step 4: Compute the loss
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-model.eval()
 
-total_loss = 0
-total_samples = 0
+class FSDPTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.repo_id = base_repo_id
+        self.api = HfApi()
 
-with torch.no_grad():
-    for batch in dataloader:
-        input_ids = torch.tensor(batch["input_ids"]).to(device)
-        attention_mask = torch.tensor(batch["attention_mask"]).to(device)
-        labels = torch.tensor(batch["labels"]).to(device)
+    def save_model(self, output_dir=None, _internal_call=False):
+        if output_dir is None:
+            output_dir = self.args.output_dir
+
+        self.save_and_push_model(output_dir)
+
+    def save_and_push_model(self, output_dir):
+        save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
         
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-        loss = outputs.loss
+        with FSDP.state_dict_type(self.model, StateDictType.FULL_STATE_DICT, save_policy):
+            cpu_state_dict = self.model.state_dict()
         
-        total_loss += loss.item() * input_ids.size(0)
-        total_samples += input_ids.size(0)
+        self.model.save_pretrained(output_dir, state_dict=cpu_state_dict)
 
-average_loss = total_loss / total_samples
-print(f"Average loss: {average_loss:.4f}")
+
+
+
+
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, attn_implementation="flash_attention_2")
+model.gradient_checkpointing_enable()
+# model.gradient_checkpointing_enable(gradient_checkpointing_kwargs={"use_reentrant": False})
+
+
+
+
+
+dataset = load_dataset(dsn, split="train")
+
+print("Dataset loaded")
+
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    accuracy = (predictions == labels).mean()
+    return {"accuracy": accuracy} 
+
+train_dataset = dataset
+training_args = TrainingArguments(
+    overwrite_output_dir=True,
+    num_train_epochs=epochs,
+    per_device_train_batch_size=batch_size, 
+    logging_steps=12,
+    fp16=True,
+    output_dir=f"./{base_repo_id}",
+    fsdp="auto_wrap",
+    report_to="wandb", 
+    save_steps=save_steps,
+    remove_unused_columns=True, 
+    learning_rate=0
+    # gradient_accumulation_steps=16,  # Adjust this value as needed
+    # learning_rate=7e-5,
+
+
+)
+
+print("Training arguments set")
+
+trainer = FSDPTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    compute_metrics=compute_metrics,  
+)
+
+
+trainer.train()
+

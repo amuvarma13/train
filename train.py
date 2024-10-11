d0931fd7d47771a8968c8c4347c3494b13f847a4
Amartya Varma <amartya.varma@gmail.com>
2024-10-10 23:33:24 -0700

    f

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "2.3m-test-0"
 project_name = "3dups"
-dsn = "amuvarma/conversation-tune-13k-25kl"
+dsn = "amuvarma/convo-fpsft-13k"
 
 model_name = "amuvarma/llama-2.3m-full"  # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
@@ -153,4 +153,4 @@ def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):
         print(f"Error during upload: {e}")
         return None
     
-push_folder_to_hub(f"./{base_repo_id}", f"amuvarma/conversationtune-13k", "Update model")
+push_folder_to_hub(f"./{base_repo_id}", f"amuvarma/conversationtune-text-13k", "Update model")

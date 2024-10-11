7c87451a3a6e57aee03bbf6e5243262b5d12e9da
Amartya Varma <amartya.varma@gmail.com>
2024-10-11 00:59:22 -0700

    f

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ base_repo_id = "2.3m-test-0"
 project_name = "3dups"
 dsn = "amuvarma/conversation_text-tune-13k-25kl"
 
-model_name = "amuvarma/llama-checkpoint-180000" # Replace with your model
+model_name = "amuvarma/llama-wdups-1node-checkpoint-96000" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
 batch_size = 1

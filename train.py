35c7e0ab905037fc1ac69a5af102a284b4514df0
Amartya Varma <amartya.varma@gmail.com>
2024-10-18 02:03:03 -0700

    f

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ dsn = "amuvarma/luna-trejo-vad-no-emo-1"
 
 model_name = "amuvarma/llama-2.3m-full" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
-epochs = 3
+epochs = 1
 batch_size = 1
 pad_token = 128263
 save_steps = 10000

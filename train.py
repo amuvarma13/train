04739e4fe128f05fd33f6d2ec44c9de45f0ed33f
Amartya Varma <amartya.varma@gmail.com>
2024-10-20 13:04:08 -0700

    f

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "2.3m-test-0"
 project_name = "catastrophic-forgetting"
-dsn = "amuvarma/50k-llama-wdups"
+dsn = "amuvarma/50k-llama-dups3-contonly"
 
 model_name = "meta-llama/Llama-3.2-3B" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"

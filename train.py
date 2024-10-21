1c37f47f566d9034dbf44515ccf20aaa5993cf93
Amartya Varma <amartya.varma@gmail.com>
2024-10-21 13:54:02 -0700

    f

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ dsn = "amuvarma/750k-content-only"
 model_name = "meta-llama/Llama-3.2-3B" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
-batch_size = 1
+batch_size = 12
 pad_token = 128263
 save_steps = 3000
 
@@ -89,7 +89,7 @@ training_args = TrainingArguments(
     overwrite_output_dir=True,
     num_train_epochs=epochs,
     per_device_train_batch_size=batch_size, 
-    logging_steps=12,
+    logging_steps=72,
     fp16=True,
     output_dir=f"./{base_repo_id}",
     fsdp="auto_wrap",

aa442e2b37e2d2a48e09d652162bc324e7fd8f54
Amartya Varma <amartya.varma@gmail.com>
2024-10-21 01:40:36 -0700

    f

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ dsn = "amuvarma/750k-content-only"
 model_name = "meta-llama/Llama-3.2-3B" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
-batch_size = 1
+batch_size = 12
 pad_token = 128263
 save_steps = 10000
 

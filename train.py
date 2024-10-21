2aeceb65a3e1f4690d9c4e36de4707d411e3f929
Amartya Varma <amartya.varma@gmail.com>
2024-10-21 13:49:55 -0700

    f

--- train.py
+++ train.py
@@ -13,9 +13,9 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "models"
 project_name = "content-only"
-dsn = "amuvarma/conversation-tune-13k-contentonly"
+dsn = "amuvarma/750k-content-only"
 
-model_name = "amuvarma/750k-content-3b" # Replace with your model
+model_name = "meta-llama/Llama-3.2-3B" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
 batch_size = 1

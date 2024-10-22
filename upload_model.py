29158ec1967a8916b9e7ac989b510f39e36d1997
Amartya Varma <amartya.varma@gmail.com>
2024-10-21 18:14:02 -0700

    f

--- upload_model.py
+++ upload_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "models/checkpoint-6000"
+base_repo_id = "models/checkpoint-156"
 
 
 def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):

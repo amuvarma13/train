8b51ee9cee1e8ef0a1e117d713072674c13bc384
Amartya Varma <amartya.varma@gmail.com>
2024-11-01 09:42:44 +0000

    F

--- upload_model.py
+++ upload_model.py
@@ -37,4 +37,4 @@ def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):
         return None
 
 
-push_folder_to_hub(f"./{base_repo_id}", f"amuvarma/750k-8b-0, "Update model")
+push_folder_to_hub(f"./{base_repo_id}", f"amuvarma/750k-8b-0", "Update model")

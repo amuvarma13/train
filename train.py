cba52f9ae24e6051b16eea576937b7ce813e7c0a
Amartya Varma <amartya.varma@gmail.com>
2024-11-01 18:26:29 +0000

    F

--- train.py
+++ train.py
@@ -13,9 +13,9 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "models"
 project_name = "contentonly-stt-tune"
-dsn = "amuvarma/750-6cols-stttts"
+dsn = "amuvarma/100k-1cols-stt-textloss"
 
-model_name = "meta-llama/Llama-3.1-8B" # Replace with your model
+model_name = "amuvarma/750k-shuffled-25-10" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
 batch_size = 3

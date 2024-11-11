03681d4593ac282493eef072fcb9dc020322ff05
Amartya Varma <amartya.varma@gmail.com>
2024-11-10 22:12:42 -0800

    f

--- upload_model.py
+++ upload_model.py
@@ -3,7 +3,7 @@ import os
 
 
 base_repo_id = "models/checkpoint-826"
-upload_name = "amuvarma/luna-3days-tagged-amufac-1"
+upload_name = "amuvarma/luna-3days-tagged-amufac-2"
 
 
 def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):

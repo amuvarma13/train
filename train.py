a6218b4b0236265d0113800fb8690bea73f36110
Amartya Varma <amartya.varma@gmail.com>
2024-11-10 21:40:37 -0800

    f

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from huggingface_hub import HfApi, create_repo
 
 base_repo_id = "models"
 project_name = "luna-tune-tts"
-dsn = "amuvarma/amu_fced_luna"
+dsn = "amuvarma/luna-full-finetune-tagged"
 
 model_name = "amuvarma/llama-2.3m-full" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"

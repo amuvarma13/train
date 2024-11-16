10c190801b88cac901286a0882e71a7261c42cc3
Amartya Varma <amartya.varma@gmail.com>
2024-11-15 17:40:42 -0800

    f

--- trainClassifier.py
+++ trainClassifier.py
@@ -3,22 +3,22 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trai
 from datasets import load_dataset
 
 
-#load model, tokeniser?, and dataset
+# load model, tokeniser?, and dataset
 
-dsn  = "amuvarma/luna-day3-classification"
+dsn = "amuvarma/luna-day3-classification"
 tkn = "meta-llama/Llama-3.2-3B"
 model_name = "amuvarma/llama-2.3m-full"
 ds = load_dataset(dsn)
 tokenizer = AutoTokenizer.from_pretrained(tkn)
-tokenizer.add_special_tokens({'additional_special_tokens': [f"[T{i}]" for i in range(9000)]})
+tokenizer.add_special_tokens(
+    {'additional_special_tokens': [f"[T{i}]" for i in range(9000)]})
 model = AutoModelForSequenceClassification.from_pretrained(
-    model_name, 
-    num_labels=8, 
+    model_name,
+    num_labels=8,
     attn_implementation="flash_attn_2",
-    )
+)
 
-ds = ds.shuffle(seed=42)   
-
+ds = ds.shuffle(seed=42)
 
 
 training_args = TrainingArguments(
@@ -39,4 +39,3 @@ trainer = Trainer(
 # Train the model
 print("Starting training...")
 trainer.train()
-

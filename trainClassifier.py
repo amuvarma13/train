62d397642737e1cf4482081142ccc3f86f641ad5
Amartya Varma <amartya.varma@gmail.com>
2024-11-15 16:49:38 -0800

    f

---
+++ trainClassifier.py
@@ -0,0 +1,39 @@
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
+from datasets import load_dataset
+
+
+#load model, tokeniser?, and dataset
+
+dsn  = "amuvarma/luna-day3-classification"
+tkn = "meta-llama/Llama-3.2-3B"
+model_name = "amuvarma/llama-2.3m-full"
+ds = load_dataset(dsn)
+ds = ds['train']
+tokenizer = AutoTokenizer.from_pretrained(tkn)
+model = AutoModelForSequenceClassification.from_pretrained(tkn)
+
+ds = ds.shuffle(seed=42)   
+
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=3,
+    per_device_train_batch_size=8,
+    logging_steps=1,
+    save_steps=0,
+    evaluation_strategy="no",
+)
+
+# Initialize Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=ds["train"],
+    tokenizer=tokenizer,
+)
+
+# Train the model
+print("Starting training...")
+trainer.train()
+

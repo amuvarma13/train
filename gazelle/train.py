f9b7b13fc1e4ba8ffa9e3eb3003ab9d12717e8c0
Amartya Varma <amartya.varma@gmail.com>
2024-11-17 19:57:28 -0800

    f

--- gazelle/train.py
+++ gazelle/train.py
@@ -17,7 +17,7 @@ dsp = preprocess_dataset(ds["dev"])
 project_name = "gazelle-projection"
 wandb.init(
     project=project_name,
-    name = "p4-17-11-dev-b4-gpu2"
+    name = "p5-17-11-dev-b4-gpu2"
 )
  
  

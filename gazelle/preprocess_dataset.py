5ea842719428e1f9ab874bb264dbac620a305f3f
Amartya Varma <amartya.varma@gmail.com>
2024-11-17 19:17:43 -0800

    f

--- gazelle/preprocess_dataset.py
+++ gazelle/preprocess_dataset.py
@@ -12,40 +12,27 @@ def pad_or_crop_audio(audio_tensor, target_length=300000):
         return torch.cat([audio_tensor, torch.zeros(target_length - len(audio_tensor))])
     return audio_tensor
 
-def preprocess_dataset(ds, batch_size=32):
+def preprocess_dataset(ds):
     ds = ds.remove_columns([col for col in ds.column_names if col not in ["transcript", "audio"]])
 
     instruction = "Read out this phrase back."
     instruction_inputs = tokenizer(instruction)
     instruction_input_ids = instruction_inputs["input_ids"]
 
-    def process_batch(examples):
-        batch_size = len(examples["audio"])
-        
-        audio_values = [
-            pad_or_crop_audio(torch.tensor(audio["array"]))
-            for audio in examples["audio"]
-        ]
-        
-        transcript_encodings = tokenizer(
-            examples["transcript"],
+    
+    def process_example(example):
+        example["audio_values"] = pad_or_crop_audio(torch.tensor(example["audio"]["array"]))
+        example["transcript_ids"] = tokenizer(
+            example["transcript"], 
             padding="max_length",
             max_length=100,
             truncation=True
-        )
-        
-        return {
-            "audio_values": audio_values,
-            "transcript_ids": transcript_encodings["input_ids"],
-            "labels": transcript_encodings["input_ids"],
-            "input_ids": [instruction_input_ids] * batch_size
-        }
+        )["input_ids"]
+        example["labels"] =  example["transcript_ids"]
+        example["input_ids"] = instruction_input_ids
+        return example
 
-    ds = ds.map(
-        process_batch,
-        batched=True,
-        batch_size=batch_size,
-        remove_columns=ds.column_names
-    )
+    ds = ds.map(process_example)
+    ds = ds.remove_columns([col for col in ds.column_names if col not in ["audio_values", "transcript_ids", "labels", "input_ids" ]])
     
     return ds

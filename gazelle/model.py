1efdc6941a780a3e9449505f3a232d51b434e0b3
Amartya Varma <amartya.varma@gmail.com>
2024-11-17 17:39:37 -0800

    f

--- gazelle/model.py
+++ gazelle/model.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 from transformers.modeling_outputs import CausalLMOutputWithPast
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B", attn_implementation="flash_attention_2")
 
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
 w2vmodel = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")

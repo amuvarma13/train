3232ea19d4d39259483ce1941790c01e95795ae8
Amartya Varma <amartya.varma@gmail.com>
2024-11-18 21:52:46 -0800

    f

--- gazelle/model.py
+++ gazelle/model.py
@@ -10,7 +10,7 @@ tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-3B")
 
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
-w2vmodel = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base", torch_dtype=torch.float16 )
+w2vmodel = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base" )
 
 class RMSNorm(nn.Module):
     def __init__(self, eps=1e-6):

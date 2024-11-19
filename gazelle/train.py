218c7c3d97155dafdfe11e780c78222303c5f831
Amartya Varma <amartya.varma@gmail.com>
2024-11-18 22:43:03 -0800

    f

--- gazelle/train.py
+++ gazelle/train.py
@@ -6,7 +6,7 @@ from preprocess_dataset import preprocess_dataset
 import wandb
 import torch
 
-dsn = "amuvarma/mls-train-200-1000"
+dsn = "amuvarma/mls-train-200"
 
 gazelle_model = GazelleLlama()
 

de9617596fe5a694744a1345bad22f32ab0ab89f
Amartya Varma <amartya.varma@gmail.com>
2024-11-18 21:18:23 -0800

    f

--- gazelle/train.py
+++ gazelle/train.py
@@ -34,7 +34,7 @@ training_args = TrainingArguments(
     report_to="wandb",  # Disables wandb and other reporting
     save_safetensors=False, 
     logging_steps=12,
-    save_steps=10,
+    save_strategy="no",
     # bf16=True,
 )
 

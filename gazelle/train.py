ece0fa05864d129afab3ffbc87ef0cddf87c74b8
Amartya Varma <amartya.varma@gmail.com>
2024-11-20 22:05:26 -0800

    f

--- gazelle/train.py
+++ gazelle/train.py
@@ -3,8 +3,10 @@ from transformers import Trainer, TrainingArguments
 from model import GazelleLlama
 from datasets import load_dataset
 from preprocess_dataset import preprocess_dataset
+from quick_load import load_dataset_parallel
 import wandb
 import torch
+import os
 
 # dsn = "amuvarma/mls-train-dev-1000-nopad"
 
@@ -12,43 +14,45 @@ dsn = "amuvarma/mls-eng-10k-200k"
 
 gazelle_model = GazelleLlama()
 
-ds = load_dataset(dsn)
+ds = load_dataset_parallel(dsn, num_proc=(os.cpu_count() - 1))
 
-# dsp = preprocess_dataset(ds["dev"])
-dsp = preprocess_dataset(ds["train"])
+print(ds)
 
-project_name = "gazelle-projection"
-wandb.init(
-    project=project_name,
-    name = "20-11-p8-8gpu-3b-lossfn-n"
-)
+
+# dsp = preprocess_dataset(ds["train"])
+
+# project_name = "gazelle-projection"
+# wandb.init(
+#     project=project_name,
+#     name = "20-11-p8-8gpu-3b-lossfn-n"
+# )
  
  
 
 
 
-# Define minimal training arguments with wandb disabled
-training_args = TrainingArguments(
-    output_dir="./gazelle-llama-output",
-    num_train_epochs=1,
-    per_device_train_batch_size=1,
-    learning_rate=4e-3,
-    report_to="wandb",  # Disables wandb and other reporting
-    save_safetensors=False, 
-    logging_steps=24,
-    save_strategy="no",
-)
+# # Define minimal training arguments with wandb disabled
+# training_args = TrainingArguments(
+#     output_dir="./gazelle-llama-output",
+#     num_train_epochs=1,
+#     per_device_train_batch_size=1,
+#     learning_rate=4e-3,
+#     report_to="wandb",  # Disables wandb and other reporting
+#     save_safetensors=False, 
+#     logging_steps=24,
+#     save_strategy="no",
+# )
 
-# Initialize trainer
-trainer = Trainer(
-    model=gazelle_model,
-    args=training_args,
-    train_dataset=dsp,
-)
+# # Initialize trainer
+# trainer = Trainer(
+#     model=gazelle_model,
+#     args=training_args,
+#     train_dataset=dsp,
+# )
 
-# Start training
-trainer.train() 
+# # Start training
+# trainer.train() 
 
-# Save model
-torch.save(gazelle_model.state_dict(), "gazelle-llama-output/model.pt")
+# # Save model
+# torch.save(gazelle_model.state_dict(), "gazelle-llama-output/model.pt")
 

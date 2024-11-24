dbdd8930218fd68b9de589183622527350c9c601
Amartya Varma <amartya.varma@gmail.com>
2024-11-23 23:18:30 -0800

    f

--- upload_model.py
+++ upload_model.py
@@ -2,8 +2,8 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "models/checkpoint-2500"
-upload_name = "amuvarma/convo-tts-tune-3"
+base_repo_id = "models/checkpoint-625"
+upload_name = "amuvarma/convo-tts-tune-4"
 
 
 def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):

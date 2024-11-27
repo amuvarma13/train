04e4d54ea270fb3985ecaaa34beb5ef857a1b0a5
Amartya Varma <amartya.varma@gmail.com>
2024-11-26 19:17:54 -0800

    f

--- interleaved_ds_train.py
+++ interleaved_ds_train.py
@@ -23,7 +23,7 @@ model_name = "meta-llama/Llama-3.2-3B" # Replace with your model
 
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
-batch_size = 1
+batch_size = 2
 number_processes = 8
 pad_token = 128263
 save_steps = 12000

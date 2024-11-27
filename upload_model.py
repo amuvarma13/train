84c7ddf2edd2f66d67c8e6f11e2d8707a4acde11
Amartya Varma <amartya.varma@gmail.com>
2024-11-27 07:30:41 -0800

    f

--- upload_model.py
+++ upload_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "checkpoint-31250"
+base_repo_id = "models/checkpoint-31250"
 upload_name = "amuvarma/1-1-interleaved-text-content-tokens-1mn-samples"
 
 

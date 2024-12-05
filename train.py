815266e47789c0d9cc9919a753e84549dff40181
Amartya Varma <amartya.varma@gmail.com>
2024-12-04 20:51:32 -0800

    f

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from huggingface_hub import HfApi, create_repo
 base_repo_id = "models"
 project_name = "tts-ify"
 
-dsn = "amuvarma/dev-qa_large_0_4_speechqa-both-full-facodec-1dups-6rows-procc"
+dsn = "amuvarma/smoltalk-audio-speech-raw-1dups-6rows-proc"
 model_name = "amuvarma/text-speech-1m-checkpoint-250000-of-250000" # Replace with your model
 
 tokenizer_name = "meta-llama/Llama-3.2-3B"

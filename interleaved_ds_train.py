c94bae62b508c282c66705bfe1d8a1cea65cd22e
Amartya Varma <amartya.varma@gmail.com>
2024-12-08 16:35:25 -0800

    f

--- interleaved_ds_train.py
+++ interleaved_ds_train.py
@@ -219,6 +219,7 @@ training_args = TrainingArguments(
     report_to="wandb", 
     save_steps=save_steps,
     remove_unused_columns=True, 
+    learning_rate=1e-4,
 
     # warmup_steps=100,
     # gradient_accumulation_steps=16,  # Adjust this value as needed

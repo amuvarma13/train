cc1fe2ab7d3e0a1c9a1cbe9c7b2ed9cad8f587f5
Amartya Varma <amartya.varma@gmail.com>
2024-12-11 17:36:51 -0800

    f

--- upload_model.py
+++ upload_model.py
@@ -2,8 +2,8 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "models/checkpoint-12000"
-upload_name = "amuvarma/snac-tts-checkpoint-12000"
+base_repo_id = "models/checkpoint-24971"
+upload_name = "amuvarma/snac-tts-checkpoint-24971"
 
 
 def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):

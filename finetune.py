a66b707af1be8fc5417255e8808e026f0550eac3
Amartya Varma <amartya.varma@gmail.com>
2024-12-23 16:27:40 +0000

    f

--- finetune.py
+++ finetune.py
@@ -40,11 +40,11 @@ model = get_peft_model(model, peft_config)
 
 trainable_params = 0
 all_param = 0
-for _, param in model.named_parameters():
-    all_param += param.numel()
-    if param.requires_grad:
-        trainable_params += param.numel()
-print(f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}")
+# for _, param in model.named_parameters():
+#     all_param += param.numel()
+#     if param.requires_grad:
+#         trainable_params += param.numel()
+# print(f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}")
 
 
 # Define training arguments

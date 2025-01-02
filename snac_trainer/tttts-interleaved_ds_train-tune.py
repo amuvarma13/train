3c41a1ffd1ea3f3a3aec3366d3ed921a9ea2e475
Amartya Varma <amartya.varma@gmail.com>
2025-01-02 19:03:36 +0000

    f

--- snac_trainer/tttts-interleaved_ds_train-tune.py
+++ snac_trainer/tttts-interleaved_ds_train-tune.py
@@ -26,7 +26,7 @@ model_name = "amuvarma/snac-pretrain-2m-96000" # Replace with your model
 tokenizer_name = "meta-llama/Llama-3.2-3B"
 epochs = 1
 batch_size = 1
-number_processes = 8
+number_processes = 2
 pad_token = 128263
 save_steps = 1675
 

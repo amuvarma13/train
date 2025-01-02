cb055603748fe435a932c5b8f5eaa910a79119ba
Amartya Varma <amartya.varma@gmail.com>
2025-01-02 00:17:02 +0000

    f

--- upload_model.py
+++ upload_model.py
@@ -2,8 +2,8 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "checkpoints/checkpoint-1430"
-upload_name = "amuvarma/zuck-tttts-qa-checkpoint-1430"
+base_repo_id = "checkpoints/checkpoint-1675"
+upload_name = "amuvarma/zuck-tttts-qa-checkpoint-1675"
 
 
 def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):

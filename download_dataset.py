186af3369952697c0d35d541d5ae1c0b1073952d
Amartya Varma <amartya.varma@gmail.com>
2025-01-15 14:39:03 -0800

    f

--- download_dataset.py
+++ download_dataset.py
@@ -8,5 +8,5 @@ snapshot_download(
     repo_id=repo_id,
     repo_type="dataset",    # important if it's a dataset repo
     revision="main",        # or a specific branch/tag/commit
-    max_workers=8           # number of parallel download threads
+    max_workers=64          # number of parallel download threads
 )

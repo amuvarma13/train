f862f72565ccdbee1d683fd40261f174a0316f6a
Amartya Varma <amartya.varma@gmail.com>
2025-01-16 11:11:03 -0800

    f

---
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -0,0 +1,12 @@
+from huggingface_hub import snapshot_download
+
+# Replace "username/dataset_name" with the actual repo ID
+repo_id = "amuvarma/proj-train-qa-and-speechqa" # e.g., "huggingface/poems_dataset"
+
+# Download the entire dataset repository
+snapshot_download(
+    repo_id=repo_id,
+    repo_type="dataset",    # important if it's a dataset repo
+    revision="main",        # or a specific branch/tag/commit
+    max_workers=64          # number of parallel download threads
+)

44dc848fef7e90d2386f96c00183212b29a0ebaf
Amartya Varma <amartya.varma@gmail.com>
2025-01-15 21:43:14 -0800

    f

--- TRAINING_SCRIPTS/upload_model.py
+++ TRAINING_SCRIPTS/upload_model.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
 base_repo_id = "checkpoints/checkpoint-120000"
-upload_name = "amuvarma/8b-2m-checkpoint-120000"
+upload_name = "amuvarma/3b-10m-checkpoint-120000"
 
 
 def upload_single_file(args):

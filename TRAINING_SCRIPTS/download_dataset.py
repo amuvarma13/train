0a053e8615090055ea7c9abc35591d2d9dc870a2
Amartya Varma <amartya.varma@gmail.com>
2025-01-16 14:21:26 -0800

    f

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,12 +1,10 @@
 from huggingface_hub import snapshot_download
 
-# Replace "username/dataset_name" with the actual repo ID
-repo_id = "amuvarma/proj-train-qa-and-speechqa" # e.g., "huggingface/poems_dataset"
+repo_id = "amuvarma/all_qas-audio"
 
-# Download the entire dataset repository
 snapshot_download(
     repo_id=repo_id,
-    repo_type="dataset",    # important if it's a dataset repo
-    revision="main",        # or a specific branch/tag/commit
-    max_workers=64          # number of parallel download threads
+    repo_type="dataset",   
+    revision="main",        
+    max_workers=64         
 )

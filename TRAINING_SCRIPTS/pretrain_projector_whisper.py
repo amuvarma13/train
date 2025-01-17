2432fa09b3d5717bd545f18f258493c957e903e5
Amartya Varma <amartya.varma@gmail.com>
2025-01-17 15:42:08 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector_whisper.py
+++ TRAINING_SCRIPTS/pretrain_projector_whisper.py
@@ -127,7 +127,7 @@ def inference_collator(audio_input, user_res, ass_res):
     mel, length = process_audio_tensor(audio_input)
     mel = mel.to(whisper_model.device)
     audio_feature = whisper_model.embed_audio(mel)[0][:length]
-    print("embedded audio feeatures shape", audio_feature)
+    print("embedded audio feeatures shape", audio_feature.shape)
 
 
     return {

1e0734a98cee0df0546be58c2283d58792de1329
Amartya Varma <amartya.varma@gmail.com>
2025-01-17 10:51:27 -0800

    f

--- TRAINING_SCRIPTS/download_model.py
+++ TRAINING_SCRIPTS/download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 # Download only model config and safetensors
 model_path = snapshot_download(
-    repo_id="amuvarma/3b-2m-checkpoint-proj3-310",
+    repo_id="amuvarma/snac-e2e-projonly-3",
     allow_patterns=[
         "config.json",
         "*.safetensors",

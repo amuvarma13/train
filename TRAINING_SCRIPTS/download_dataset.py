d41e8a482a58f6b74678274f784ef659a79e2919
Amartya Varma <amartya.varma@gmail.com>
2025-01-17 14:14:02 -0800

    f

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/mls-eng-10k-500k-projection_prep"
+repo_id = "TRAINING_SCRIPTS/FINETUNE_PROJECTOR_ARGS.yaml"
 
 snapshot_download(
     repo_id=repo_id,

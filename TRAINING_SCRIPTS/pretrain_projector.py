0e16322eef4798c37dec8fa5960510f44226df9c
Amartya Varma <amartya.varma@gmail.com>
2025-01-17 14:53:39 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector.py
+++ TRAINING_SCRIPTS/pretrain_projector.py
@@ -139,6 +139,8 @@ class AudioChatDataCollator:
             user_response = "<|audio|>"
             
         
+        print(f"User Response: {user_response}")
+        
 
         batch = inference_collator(audio, user_response, assistant_response)
 

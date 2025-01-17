ea6c916d9263d1cc00f623a742a0c87be8d78f0f
Amartya Varma <amartya.varma@gmail.com>
2025-01-16 18:23:10 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector.py
+++ TRAINING_SCRIPTS/pretrain_projector.py
@@ -129,12 +129,12 @@ class AudioChatDataCollator:
 
     def __call__(self, features):
         audio = torch.tensor([features[0]["audio"]["array"]])
-        assistant_response = features[0]["answer"]
-        user_response = features[0]["question"]
+        assistant_response = features[0]["assistant"]
+        user_response = features[0]["user"]
 
         # Simple contains check
         if "<|audio|>" in user_response:
-            user_response = features[0]["question"]
+            user_response = features[0]["user"]
         else:
             user_response = "<|audio|>"
             

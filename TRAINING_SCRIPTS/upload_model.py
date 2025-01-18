f55690febd83f46ded2e7dd8604dcf37eeaac0b1
Amartya Varma <amartya.varma@gmail.com>
2025-01-17 17:16:45 -0800

    f

--- TRAINING_SCRIPTS/upload_model.py
+++ TRAINING_SCRIPTS/upload_model.py
@@ -3,8 +3,8 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
-base_repo_id = "checkpoints/checkpoint-288000"
-upload_name = "amuvarma/8b-2m-pretrain"
+base_repo_id = "checkpoints/checkpoint-7813"
+upload_name = "amuvarma/3b-2m-whisper-proj-pre"
 
 
 def upload_single_file(args):

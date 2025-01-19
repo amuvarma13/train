a72c86c1eb26fa60788eab8fad1f1e59a770981e
Amartya Varma <amartya.varma@gmail.com>
2025-01-18 18:44:31 -0800

    f

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,8 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/snacced-zuck-convo-sttsed-proc-no-discrepancies"
-
+repo_id = "amuvarma/textonlylabels-snac-sts-convo-zuck"
 snapshot_download(
     repo_id=repo_id,
     repo_type="dataset",   

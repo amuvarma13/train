ed29639c177275f94e1ff57dca893f1860308f44
Amartya Varma <amartya.varma@gmail.com>
2025-01-18 17:08:57 -0800

    f

--- TRAINING_SCRIPTS/gzf/modeling_gazelle_whisper.py
+++ TRAINING_SCRIPTS/gzf/modeling_gazelle_whisper.py
@@ -575,9 +575,11 @@ class GazelleForConditionalGeneration(GazellePreTrainedModel):
                     audio_features, inputs_embeds, input_ids, attention_mask, labels
                 )
 
-                print("inputs_embeds.shape",inputs_embeds.shape[1])
-                print(input_ids.shape, audio_values.shape, audio_features.shape)
-                print(input_ids.shape[1] - audio_values.shape[0] + audio_features.shape[1]*audio_values.shape[0])
+                count = (labels == -100).sum().item()
+                print("Number of entries equal to -100:", count, audio_features.shape[0]*audio_features.shape[1])
+                # print("inputs_embeds.shape",inputs_embeds.shape[1])
+                # print(input_ids.shape, audio_values.shape, audio_features.shape)
+                # print(input_ids.shape[1] - audio_values.shape[0] + audio_features.shape[1]*audio_values.shape[0])
 
                 if labels is None:
                     labels = torch.full_like(

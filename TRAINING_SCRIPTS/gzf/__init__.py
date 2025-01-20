62206eb99024c0fbc9beee30148bd7fb1b8ef42b
Amartya Varma <amartya.varma@gmail.com>
2025-01-20 10:46:30 -0800

    f

--- TRAINING_SCRIPTS/gzf/__init__.py
+++ TRAINING_SCRIPTS/gzf/__init__.py
@@ -1,5 +1,5 @@
 """Gazelle is a joint speech-language model for end-to-end voice conversation."""
-from .modeling_gazelle import (
+from .modelling_gazelle_whisper import (
     GazelleConfig,
     GazelleForConditionalGeneration,
     GazellePreTrainedModel,

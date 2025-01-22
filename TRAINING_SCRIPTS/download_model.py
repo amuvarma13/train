296ee0f120f340afac3b56c604007f12ee7bcf06
Amartya Varma <amartya.varma@gmail.com>
2025-01-21 21:57:20 -0800

    f

--- TRAINING_SCRIPTS/download_model.py
+++ TRAINING_SCRIPTS/download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 # Download only model config and safetensors
 model_path = snapshot_download(
-    repo_id='amuvarma/8b-zuckqa-TTTTS',
+    repo_id='amuvarma/zuckconvotune-3b-2m-checkpoint-124',
     allow_patterns=[
         "config.json",
         "*.safetensors",

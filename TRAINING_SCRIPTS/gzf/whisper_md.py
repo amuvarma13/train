c8ee9f9f4c6d052574508cbc61386af717bcd73c
Amartya Varma <amartya.varma@gmail.com>
2025-01-22 10:53:18 -0800

    f

--- TRAINING_SCRIPTS/gzf/whisper_md.py
+++ TRAINING_SCRIPTS/gzf/whisper_md.py
@@ -71,7 +71,7 @@ class GazelleConfig(PretrainedConfig):
         audio_token_index=32000,
         vocab_size=32000,
         hidden_size=4096,
-        stack_factor=4,
+        stack_factor=8,
         projector_type="mlp",
         **kwargs,
     ):

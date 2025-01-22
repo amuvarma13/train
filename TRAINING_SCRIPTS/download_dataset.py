a0d87a1e6e9fbe87f9a816ec0b8e63c951e58a4c
Amartya Varma <amartya.varma@gmail.com>
2025-01-21 19:27:35 -0800

    f

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,7 +1,8 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/snacced-flat-zuck-convo-StTtS"
+repo_id = "amuvarma/mls-eng-10k-500k-projection_prep"
+
 snapshot_download(
     repo_id=repo_id,
     repo_type="dataset",   

5ccc52f359787624aa64fbebddf44bdab65b2645
Amartya Varma <amartya.varma@gmail.com>
2025-01-23 15:49:05 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector_whisper.py
+++ TRAINING_SCRIPTS/pretrain_projector_whisper.py
@@ -50,7 +50,7 @@ device = "cpu"
 dtype = torch.float32
 if torch.cuda.is_available():
     device = "cuda"
-    dtype = torch.bfloat16
+    dtype = torch.float16
     print(f"Using {device} device")
 elif torch.backends.mps.is_available():
     device = "mps"

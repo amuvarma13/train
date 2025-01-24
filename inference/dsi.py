7e8aaebd22527eeecae82e49f287cf5cbfb8f7a0
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 10:23:23 -0800

    f

--- inference/dsi.py
+++ inference/dsi.py
@@ -1,13 +1,19 @@
-import deepspeed
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-import torch.distributed as dist
-if dist.is_initialized():
-    print(f"Rank: {dist.get_rank()}, World Size: {dist.get_world_size()}")
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 mdn = "meta-llama/Llama-3.2-3B-Instruct"
-from mii import pipeline
-pipe = pipeline(mdn)
-output = pipe(["Hello, my name is", "DeepSpeed is"], max_new_tokens=128)
-print(output)
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from accelerate import infer_auto_device_map, init_inference
+
+model_name = "meta-llama/Llama-3.2-3B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(mdn)
+model = AutoModelForCausalLM.from_pretrained(mdn, device_map="auto", torch_dtype="auto")
+
+# Initialize inference
+model = init_inference(model, device_map="auto", dtype="auto")
+
+# Inference
+inputs = tokenizer("Input String", return_tensors="pt").to(model.device)
+with torch.no_grad():
+    outputs = model.generate(**inputs)
+print(tokenizer.decode(outputs[0]))

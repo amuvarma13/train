2cdff88d4e1298142a56850fb298c3e93de529c7
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 11:38:34 -0800

    f

--- inference/main.py
+++ inference/main.py
@@ -32,6 +32,6 @@ def generate_output(prompt, llm, sampling_params):
     return generated_text
 
 prompt = "Here is a short story about a dragon:"
-sampling_params = SamplingParams(max_new_tokens=500)
+sampling_params = SamplingParams(temperature=0.5, max_tokens=500)
 generate_output(prompt, llm, sampling_params)
 

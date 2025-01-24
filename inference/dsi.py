8c9ff583fa9cbb021383516d9da36de75dcbdbd6
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 10:24:57 -0800

    f

--- inference/dsi.py
+++ inference/dsi.py
@@ -3,14 +3,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 mdn = "meta-llama/Llama-3.2-3B-Instruct"
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from accelerate import infer_auto_device_map, init_inference
 
 model_name = "meta-llama/Llama-3.2-3B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(mdn)
 model = AutoModelForCausalLM.from_pretrained(mdn, device_map="auto", torch_dtype="auto")
 
 # Initialize inference
-model = init_inference(model, device_map="auto", dtype="auto")
+
 
 # Inference
 inputs = tokenizer("Input String", return_tensors="pt").to(model.device)

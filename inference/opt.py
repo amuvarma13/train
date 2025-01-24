d3cc0696cff49f44c8be4bc27ea7f78cf5760832
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 11:03:47 -0800

    f

--- inference/opt.py
+++ inference/opt.py
@@ -2,11 +2,22 @@ from transformers import AutoTokenizer
 from optimum.onnxruntime import ORTModelForCausalLM
 
 model_name = "meta-llama/Llama-3.2-3B-Instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Make sure to disable the fast tokenizer if you run into tokenizer-related issues,
+# especially for LLaMA-based models
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
+
+# Export the model to ONNX
 ort_model = ORTModelForCausalLM.from_pretrained(model_name, export=True)
+
+# Disable use of cache in the config
 ort_model.config.use_cache = False
+
 input_text = "Here is a short story about a dragon:"
 inputs = tokenizer(input_text, return_tensors="np")
-outputs = ort_model.generate(**inputs, max_new_tokens=500)
+
+# Also explicitly pass `use_cache=False` here:
+outputs = ort_model.generate(**inputs, max_new_tokens=100, use_cache=False)
+
 generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(generated_text)

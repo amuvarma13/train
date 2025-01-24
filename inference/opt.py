01d306a425a92f3411e9abb63743f3c4b1018279
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 10:57:03 -0800

    f

--- inference/opt.py
+++ inference/opt.py
@@ -1,20 +1,18 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer
 from optimum.onnxruntime import ORTModelForCausalLM
 import torch
 
 model_name = "meta-llama/Llama-3.2-3B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
 providers = ["CUDAExecutionProvider"] if torch.cuda.is_available() else ["CPUExecutionProvider"]
 ort_model = ORTModelForCausalLM.from_pretrained(
-    model,
+    model_name,
     export=True,
     optimization_level="basic",
     providers=providers
 )
 input_text = "Here is a short story about a dragon:"
-inputs = tokenizer(input_text, return_tensors="pt")
-with torch.no_grad():
-    outputs = ort_model.generate(**inputs, max_new_tokens=500)
+inputs = tokenizer(input_text, return_tensors="np")
+outputs = ort_model.generate(**inputs, max_new_tokens=500)
 generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(generated_text)

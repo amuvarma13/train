54a030380228856bcf239d5593ce894b0f7ac876
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 09:48:11 -0800

    f

---
+++ inference/dsi.py
@@ -0,0 +1,27 @@
+from mii import pipeline
+mdn = "meta-llama/Llama-3.1-8B-Instruct"
+model_path = snapshot_download(
+    repo_id=mdn,
+    allow_patterns=[
+        "config.json",
+        "*.safetensors",
+        "model.safetensors.index.json",
+    ],
+    ignore_patterns=[
+        "optimizer.pt",
+        "pytorch_model.bin",
+        "training_args.bin",
+        "scheduler.pt",
+        "tokenizer.json",
+        "tokenizer_config.json",
+        "special_tokens_map.json",
+        "vocab.json",
+        "merges.txt",
+        "tokenizer.*"
+    ]
+)
+
+pipe = pipeline(mdn)
+output = pipe(["Hello, my name is", "DeepSpeed is"], max_new_tokens=128)
+
+print(output)

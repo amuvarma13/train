19bf50123c5d4a2ffd0384c0c959622416e90f4d
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 10:11:10 -0800

    f

--- inference/dsi.py
+++ inference/dsi.py
@@ -1,26 +1,16 @@
-from mii import pipeline
+import deepspeed
 from huggingface_hub import snapshot_download
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 mdn = "meta-llama/Llama-3.2-3B-Instruct"
-model_path = snapshot_download(
-    repo_id=mdn,
-    allow_patterns=[
-        "config.json",
-        "*.safetensors",
-        "model.safetensors.index.json",
-        "tokenizer.json",
-        "tokenizer_config.json", 
-        "special_tokens_map.json"
-    ],
-    ignore_patterns=[
-        "optimizer.pt",
-        "pytorch_model.bin",
-        "training_args.bin",
-        "scheduler.pt"
-    ]
-)
 
-# Set q_ratio to a supported value, e.g., 2
-pipe = pipeline(mdn, tensor_parallel=1, q_ratio=2)
-output = pipe(["Hello, my name is", "DeepSpeed is"], max_new_tokens=128)
-print(output)
+model = AutoModelForCausalLM.from_pretrained(mdn)
+
+ds_engine = deepspeed.init_inference(model,
+                                 tensor_parallel={"tp_size": 2},
+                                 dtype=torch.half,
+                                 checkpoint=None,
+                                 replace_with_kernel_inject=True)
+model = ds_engine.module
+output = model('Input String')

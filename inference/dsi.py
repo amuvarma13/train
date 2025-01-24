94c86d30249154c211f793ad1a08db6ca13a246e
Amartya Varma <amartya.varma@gmail.com>
2025-01-24 10:02:00 -0800

    f

--- inference/dsi.py
+++ inference/dsi.py
@@ -1,5 +1,6 @@
 from mii import pipeline
 from huggingface_hub import snapshot_download
+
 mdn = "meta-llama/Llama-3.2-3B-Instruct"
 model_path = snapshot_download(
     repo_id=mdn,
@@ -7,22 +8,18 @@ model_path = snapshot_download(
         "config.json",
         "*.safetensors",
         "model.safetensors.index.json",
+        "tokenizer.json",
+        "tokenizer_config.json", 
+        "special_tokens_map.json"
     ],
     ignore_patterns=[
         "optimizer.pt",
         "pytorch_model.bin",
         "training_args.bin",
-        "scheduler.pt",
-        "tokenizer.json",
-        "tokenizer_config.json",
-        "special_tokens_map.json",
-        "vocab.json",
-        "merges.txt",
-        "tokenizer.*"
+        "scheduler.pt"
     ]
 )
 
 pipe = pipeline(mdn)
 output = pipe(["Hello, my name is", "DeepSpeed is"], max_new_tokens=128)
-
 print(output)

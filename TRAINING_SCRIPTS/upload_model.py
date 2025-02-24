986acab812ea38cb175acd0a0dc52f3dd787ac5b
Amartya Varma <amartya.varma@gmail.com>
2025-02-23 20:53:11 -0800

    r

--- TRAINING_SCRIPTS/upload_model.py
+++ TRAINING_SCRIPTS/upload_model.py
@@ -3,7 +3,7 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
-base_repo_id = "checkpoints/checkpoint-2838"
+base_repo_id = "checkpoints/checkpoint-4735"
 upload_name = "amuvarma/luna-tts-tags"
 
 

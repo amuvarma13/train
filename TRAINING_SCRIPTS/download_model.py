502139af631166580c0f0e5a66ba142efad3c4e4
Amartya Varma <amartya.varma@gmail.com>
2025-02-24 12:59:43 -0800

    r

--- TRAINING_SCRIPTS/download_model.py
+++ TRAINING_SCRIPTS/download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 # Download only model config and safetensors
 model_path = snapshot_download(
-    repo_id="amuvarma/3b-10m-pretrain-full",
+    repo_id="amuvarma/luna-tts-tags",
     allow_patterns=[
         "config.json",
         "*.safetensors",

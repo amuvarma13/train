c076d672556177e97160568be752da392ec07009
Amartya Varma <amartya.varma@gmail.com>
2025-02-26 11:34:49 -0800

    f

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/mls-eng-10k-500k-projection_prep"
+repo_id = "amuvarma/text-messages-6m-processed-1-2g-8192l"
 
 snapshot_download(
     repo_id=repo_id,

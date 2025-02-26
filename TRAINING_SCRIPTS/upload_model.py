346c3d2b6686b0c7076224dde0adf9e135bda2b4
Amartya Varma <amartya.varma@gmail.com>
2025-02-25 20:12:44 -0800

    f

--- TRAINING_SCRIPTS/upload_model.py
+++ TRAINING_SCRIPTS/upload_model.py
@@ -3,8 +3,8 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
-base_repo_id = "checkpoints/checkpoint-1676"
-upload_name = "amuvarma/luna-rude-text-tune-ext"
+base_repo_id = "checkpoints/checkpoint-136"
+upload_name = "amuvarma/luna-rude-text-tune-ext-convo"
 
 
 def upload_single_file(args):

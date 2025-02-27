e26ec1d80b1ff357056a4d6d898b2eef0a924dc3
Amartya Varma <amartya.varma@gmail.com>
2025-02-26 17:14:41 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector_package.py
+++ TRAINING_SCRIPTS/pretrain_projector_package.py
@@ -5,7 +5,7 @@ model_name = "checkpoints/checkpoint-7813" # from stage_2_train.py
 orpheus = OrpheusTrainer(    
     stage = "stage_4",
     model_name = model_name,
-    batch_size = 21, # use batch_size * number_of_gpus = 64 for quickest training
+    batch_size = 8, # use batch_size * number_of_gpus = 64 for quickest training
     )
 
 orpheus_trainer = orpheus.create_trainer( report_to="wandb" )

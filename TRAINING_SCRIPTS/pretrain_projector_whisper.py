bd21604c42958fec43aafd358c7da8558f239cd8
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 15:06:37 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector_whisper.py
+++ TRAINING_SCRIPTS/pretrain_projector_whisper.py
@@ -108,7 +108,6 @@ def inference_collator(audio_input, user_res, ass_res):
 
     user_input_ids = tokenizer(user_res, return_tensors="pt").input_ids
 
-    print("user_input_ids", user_input_ids)
     assistant_input_ids = tokenizer(ass_res, return_tensors="pt").input_ids
 
     start_token = torch.tensor([[128259]], dtype=torch.int64)

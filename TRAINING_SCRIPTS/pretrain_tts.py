36c092c35054e54f8b16e07327975205016b3ceb
Amartya Varma <amartya.varma@gmail.com>
2025-02-27 23:19:58 -0800

    f

--- TRAINING_SCRIPTS/pretrain_tts.py
+++ TRAINING_SCRIPTS/pretrain_tts.py
@@ -11,7 +11,7 @@ import yaml
 import wandb
 from huggingface_hub import HfApi
 
-config_file = "PRETRAINTTS_ARGS-3b-10m.yaml"
+config_file = "PRETRAIN_ARGSTTS-3b-10m.yaml"
 
 with open(config_file, "r") as file:
     config = yaml.safe_load(file)

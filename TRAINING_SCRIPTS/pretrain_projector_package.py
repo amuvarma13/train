e0b70fb86fb4cb5fd65811e1502ddd5c869c1736
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 13:57:56 -0800

    f

--- TRAINING_SCRIPTS/pretrain_projector_package.py
+++ TRAINING_SCRIPTS/pretrain_projector_package.py
@@ -1,9 +1,9 @@
 from orpheus import OrpheusTrainer
-model_name = "amuvarma/kok-convo-595-12000-stage3" # from stage_2_train.py
+model_name = "meta-llama/Llama-3.2-3B" # from stage_2_train.py
 
 #** loading the datasets can take a while, even up to an hour **
 orpheus = OrpheusTrainer(    
-    stage = "stage_4",
+    stage = "stage_3",
     model_name = model_name,
     batch_size = 32, # use batch_size * number_of_gpus = 64 for quickest training
     )

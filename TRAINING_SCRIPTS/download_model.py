85458c7aafd7d167d8e253a0bbd9a55c9838be07
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 20:05:52 -0800

    f

--- TRAINING_SCRIPTS/download_model.py
+++ TRAINING_SCRIPTS/download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 # Download only model config and safetensors
 model_path = snapshot_download(
-    repo_id="google/gemma-2-9b-it",
+    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
     allow_patterns=[
         "config.json",
         "*.safetensors",

6341589371f128bc08993ec978b44cd930e8ef8c
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 19:11:18 -0800

    f

---
+++ GemmaTrain/pretrain_tts.py
@@ -0,0 +1,136 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, Trainer, TrainingArguments, AutoTokenizer
+import numpy as np
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullStateDictConfig
+from torch.distributed.fsdp import (
+    FullyShardedDataParallel as FSDP, FullStateDictConfig, StateDictType)
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.distributed import DistributedSampler
+import yaml
+import wandb
+from huggingface_hub import HfApi
+
+config_file = "PRETRAIN_ARGSTTS-3b-10m.yaml"
+
+with open(config_file, "r") as file:
+    config = yaml.safe_load(file)
+
+dsn1 = config["TTS_dataset"]
+
+
+model_name = config["model_name"]
+tokenizer_name = config["tokenizer_name"]
+
+run_name = config["run_name"]
+project_name = config["project_name"]
+base_repo_id = config["save_folder"]
+
+epochs = config["epochs"]
+batch_size = config["batch_size"]
+save_steps = config["save_steps"]
+pad_token = config["pad_token"]
+number_processes = config["number_processes"]
+learning_rate = config["learning_rate"]
+
+
+
+class FSDPTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.repo_id = base_repo_id
+        self.api = HfApi()
+
+    def save_model(self, output_dir=None, _internal_call=False):
+        if output_dir is None:
+            output_dir = self.args.output_dir
+        self.save_and_push_model(output_dir)
+
+    def save_and_push_model(self, output_dir):
+        save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
+        with FSDP.state_dict_type(self.model, StateDictType.FULL_STATE_DICT, save_policy):
+            cpu_state_dict = self.model.state_dict()
+        self.model.save_pretrained(output_dir, state_dict=cpu_state_dict)
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    accuracy = (predictions == labels).mean()
+    return {"accuracy": accuracy}
+
+
+def data_collator(features):
+    # max_length = 6144
+    input_ids = [f["input_ids"] for f in features]
+
+    if any("attention_mask" not in f for f in features):
+        attention_mask = [[1]*len(ids) for ids in input_ids]
+    else:
+        attention_mask = [f["attention_mask"] for f in features]
+
+    if any("labels" not in f for f in features):
+        labels = input_ids
+    else:
+        labels = [f["labels"] for f in features]
+
+
+    # input_ids = [ids[:max_length] for ids in input_ids]
+    # attention_mask = [m[:max_length] for m in attention_mask]
+    # labels = [l[:max_length] for l in labels]
+
+    # Convert all lists to tensors and pad
+    input_ids = torch.nn.utils.rnn.pad_sequence([torch.tensor(
+        i, dtype=torch.long) for i in input_ids], batch_first=True, padding_value=pad_token)
+    attention_mask = torch.nn.utils.rnn.pad_sequence([torch.tensor(
+        m, dtype=torch.long) for m in attention_mask], batch_first=True, padding_value=0)
+    labels = torch.nn.utils.rnn.pad_sequence([torch.tensor(
+        l, dtype=torch.long) for l in labels], batch_first=True, padding_value=-100)
+
+    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
+
+
+wandb.init(project=project_name, name=run_name)
+
+
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, attn_implementation="flash_attention_2")
+
+
+number_add_tokens = 7 * 4096 + 10
+new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]
+tokenizer.add_tokens(new_tokens)
+model.resize_token_embeddings(len(tokenizer))
+
+
+ds1 = load_dataset(dsn1, split="train")
+
+batch_total = batch_size * number_processes
+
+
+training_args = TrainingArguments(
+    overwrite_output_dir=True,
+    num_train_epochs=epochs,
+    per_device_train_batch_size=batch_size,
+    logging_steps=1,
+    fp16=True,
+    output_dir=f"./{base_repo_id}",
+    fsdp="auto_wrap",
+    report_to="wandb",
+    save_steps=save_steps,
+    remove_unused_columns=True,
+    learning_rate=learning_rate,
+    lr_scheduler_type="cosine"
+)
+
+
+trainer = FSDPTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=ds1,
+    compute_metrics=compute_metrics,
+    data_collator=data_collator,
+)
+
+trainer.train()

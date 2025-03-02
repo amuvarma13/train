664aecce087a95f5e1b3cf458a8207e4088fe0c0
Amartya Varma <amartya.varma@gmail.com>
2025-03-02 12:03:47 -0800

    f

--- GemmaTrain/download_dataset.py
+++ GemmaTrain/download_dataset.py
@@ -1,14 +1,14 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/emilia-snac-merged-all-gemma-TTS-grouped-2656"
+repo_id = "amuvarma/all-texts-2048-iids"
 
-# snapshot_download(
-#     repo_id=repo_id,
-#     repo_type="dataset",   
-#     revision="main",        
-#     max_workers=64,     
-# ) 
+snapshot_download(
+    repo_id=repo_id,
+    repo_type="dataset",   
+    revision="main",        
+    max_workers=64,     
+) 
 
 ds = load_dataset(repo_id, split="train")
 print(ds)

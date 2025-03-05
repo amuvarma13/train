077e4be88115d396fdc9254a3b5a79f6b451914b
Amartya Varma <amartya.varma@gmail.com>
2025-03-05 11:29:31 -0800

    f

--- TRAINING_SCRIPTS/upload_model.py
+++ TRAINING_SCRIPTS/upload_model.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
 base_repo_id = "checkpoints/checkpoint-2641"
-upload_name = "amuvarma/brian-luna-w_emotags"
+upload_name = "amuvarma/brian-luna-w_emotags-nowhisp"
 
 
 def upload_single_file(args):

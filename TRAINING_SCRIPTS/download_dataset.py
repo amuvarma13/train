ad09e4d9ae04d7fe0874a67ee6acac5ae758d8e7
Amartya Varma <amartya.varma@gmail.com>
2025-03-08 18:01:02 -0800

    initial website

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -7,7 +7,7 @@ snapshot_download(
     repo_id=repo_id,
     repo_type="dataset",   
     revision="main",        
-    max_workers=64,     
+    max_workers=10,     
 ) 
 
 load_dataset(repo_id, split="train")

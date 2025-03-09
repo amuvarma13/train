8794f6e1f1433c55af4b510dae4c35f9a5778f0b
Amartya Varma <amartya.varma@gmail.com>
2025-03-08 17:47:40 -0800

    initial website

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/emilia-snac-merged-18m-mod7-TTS-8192"
+repo_id = "amuvarma/podcast-all-TTS-8192"
 
 snapshot_download(
     repo_id=repo_id,

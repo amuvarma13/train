3bfc93e5808d6f4d7c043e3509508896c410291c
Amartya Varma <amartya.varma@gmail.com>
2025-03-21 01:28:02 -0700

    f

--- distill_model.py
+++ distill_model.py
@@ -63,7 +63,7 @@ class DistillationTrainer(Trainer):
 
 
 
-        temperature = 4.0
+        temperature = 8.0
         student_logits_temp = student_logits / temperature
         teacher_logits_temp = teacher_logits / temperature
 

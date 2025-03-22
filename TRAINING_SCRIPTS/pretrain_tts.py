9bf83548bfc208857c2df7710b10d3fb17f9cc0c
Amartya Varma <amartya.varma@gmail.com>
2025-03-21 22:38:02 -0700

    f

--- TRAINING_SCRIPTS/pretrain_tts.py
+++ TRAINING_SCRIPTS/pretrain_tts.py
@@ -114,7 +114,7 @@ new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]
 tokenizer.add_tokens(new_tokens)
 model.resize_token_embeddings(len(tokenizer))
 
-
+model = model.eval()
 
 
 batch_total = batch_size * number_processes

022f6a7a3ce88a8cc4c2422ec21a88d31d0a0e13
Amartya Varma <amartya.varma@gmail.com>
2025-03-22 01:37:19 -0700

    f

--- TRAINING_SCRIPTS/download_dataset.py
+++ TRAINING_SCRIPTS/download_dataset.py
@@ -1,13 +1,13 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset
 
-repo_id = "amuvarma/emilia-snac-merged-18m-smol-dedup-8192"
+repo_id = "amuvarma/em-EN-TTS-full-8192"
 
 snapshot_download(
     repo_id=repo_id,
     repo_type="dataset",   
     revision="main",        
-    max_workers=8,     
+    max_workers=64,     
 ) 
 
 load_dataset(repo_id, split="train")

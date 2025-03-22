02db5be1f88aff4b0bead8950d9fa58609a031ae
Amartya Varma <amartya.varma@gmail.com>
2025-03-22 02:23:42 -0700

    f

--- setfit_distill.py
+++ setfit_distill.py
@@ -2,14 +2,14 @@ from setfit import DistillationTrainer
 from transformers import TrainingArguments
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from setfit import SetFitModel, Trainer, TrainingArguments, sample_dataset
 
 teacher_model_name = "canopylabs/orpheus-3b-0.1-ft"
 student_model_name = "amuvarma/1b-tts-pretrain-checkpoint-108493-of-108493"
 
-teacher_model = AutoModelForCausalLM.from_pretrained(teacher_model_name, attn_implementation="flash_attention_2")
-teacher_model.eval()  # Freeze teacher parameters
+teacher_model = SetFitModel.from_pretrained(teacher_model_name, attn_implementation="flash_attention_2")
 
-model = AutoModelForCausalLM.from_pretrained(student_model_name, attn_implementation="flash_attention_2")
+model = SetFitModel.from_pretrained(student_model_name, attn_implementation="flash_attention_2")
 teacher_model.resize_token_embeddings(model.config.vocab_size)  # Resize student embeddings to match teacher's vocabulary size
 
 tokenizer = AutoTokenizer.from_pretrained(teacher_model_name)
